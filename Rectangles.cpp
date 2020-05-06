5c2e1e866b906c473bfa31c7b049b0e68d5b5481
Sevo98 <sevo98.sk@gmail.com>
2020-05-06 22:29:16 +0700

    Add random input in Rectangles

--- Rectangles.cpp
+++ Rectangles.cpp
@@ -6,50 +6,73 @@ using namespace std;
 
 void ReadRectanglesFromConsole(Rectangles& rectangle)
 {
-	bool CheckLength = false;
-	while (CheckLength == false)
+	cout << "Задать значения самостоятельно или программно? \n1. Самостоятельно\n2.Программно" << endl;
+	int Switch;
+	cin >> Switch;
+	switch (Switch)
 	{
-		try
-		{
-			cout << "Введите длину прямоугольника: ";
-			double Length;
-			CheckInputDouble(&Length);
-			if (Length <= 0)
+	case 1:
+	{
+		bool CheckLength = false;
+			while (CheckLength == false)
 			{
-				throw exception("Длина не может быть меньше 0 или равна 0! Повторите ввод.");
+				try
+				{
+					cout << "Введите длину прямоугольника: ";
+					double Length;
+					CheckInputDouble(&Length);
+					if (Length <= 0)
+					{
+						throw exception("Длина не может быть меньше 0 или равна 0! Повторите ввод.");
+					}
+					rectangle.Length = Length;
+					CheckLength = true;
+				}
+				catch (const exception&)
+				{
+					cout << "Длина не может быть меньше 0 или равна 0! Повторите ввод." << endl;
+				}
 			}
-			rectangle.Length = Length;
-			CheckLength = true;
-		}
-		catch (const exception&)
-		{
-			cout << "Длина не может быть меньше 0 или равна 0! Повторите ввод." << endl;
-		}
+
+			bool CheckWidth = false;
+			while (CheckWidth == false)
+			{
+				try
+				{
+					cout << "\nВведите ширину прямоугольника: ";
+					double Width;
+					CheckInputDouble(&Width);
+					if (Width <= 0)
+					{
+						throw exception("Ширина не может быть меньше 0 или равна 0! Повторите ввод.");
+					}
+					rectangle.Width = Width;
+					CheckWidth = true;
+				}
+				catch (const exception&)
+				{
+					cout << "Ширина не может быть меньше 0 или равна 0! Повторите ввод." << endl;
+				}
+			}
+
+			cout << "Введите координаты центра прямоугольника:" << endl;
+			ReadPointFromConsole(rectangle.point);
+	} break;
+
+	case 2:
+	{
+		double max = 1000.45654;
+		double min = -1000.45654;
+		rectangle.Length = (double)(rand()) / RAND_MAX * max;
+		rectangle.Width = (double)(rand()) / RAND_MAX * max;
+		rectangle.point.X = (double)(rand()) / RAND_MAX * (max - min);
+		rectangle.point.Y = (double)(rand()) / RAND_MAX * (max - min);
+	} break;
+
 	}
 
-	bool CheckWidth = false;
-	while (CheckWidth == false)
-	{
-		try
-		{
-			cout << "\nВведите ширину прямоугольника: ";
-			double Width;
-			CheckInputDouble(&Width);
-			if (Width <= 0)
-			{
-				throw exception("Ширина не может быть меньше 0 или равна 0! Повторите ввод.");
-			}
-			rectangle.Width = Width;
-			CheckWidth = true;
-		}
-		catch (const exception&)
-		{
-			cout << "Ширина не может быть меньше 0 или равна 0! Повторите ввод." << endl;
-		}
-	}
 
-	cout << "Введите координаты центра прямоугольника:" << endl;
-	ReadPointFromConsole(rectangle.point);
+	
 }
 
 void WriteRectanglesFromConsole(Rectangles& rectangle)
